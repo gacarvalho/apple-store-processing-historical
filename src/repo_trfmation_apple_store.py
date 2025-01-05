7c34cad51dd0b5c0105c477c71b4a02e4a09b4f9
Gabriel Carvalho <gacarvalho.contato@gmail.com>
2025-01-05 13:28:20 -0300

    add env pre e prd

--- src/repo_trfmation_apple_store.py
+++ src/repo_trfmation_apple_store.py
@@ -14,47 +14,50 @@ except ModuleNotFoundError:
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
+
+
+    # Criação da sessão Spark
+    spark = spark_session()
+
     try:
-        # Criação da sessão Spark
-        with spark_session() as spark:
-            # Coleta de métricas
-            metrics_collector = MetricsCollector(spark)
-            metrics_collector.start_collection()
+        # Coleta de métricas
+        metrics_collector = MetricsCollector(spark)
+        metrics_collector.start_collection()
 
-            # Definindo caminhos
-            datePath = datetime.now().strftime("%Y%m%d")
-            format = "parquet"
-            pathSource = f"/santander/bronze/compass/reviews/appleStore/*/odate={datePath}/*.{format}"
-            path_target = f"/santander/silver/compass/reviews/appleStore/odate={datePath}/"
-            path_target_fail = f"/santander/silver/compass/reviews_fail/appleStore/odate={datePath}/"
+        # Definindo caminhos
+        datePath = datetime.now().strftime("%Y%m%d")
+        format = "parquet"
+        pathSource = f"/santander/bronze/compass/reviews/appleStore/*/odate={datePath}/*.{format}"
+        path_target = f"/santander/silver/compass/reviews/appleStore/odate={datePath}/"
+        path_target_fail = f"/santander/silver/compass/reviews_fail/appleStore/odate={datePath}/"
 
-            # Leitura do arquivo Parquet
-            df = spark.read.parquet(pathSource).withColumn("app", regexp_extract(input_file_name(), "/appleStore/(.*?)/odate=", 1))
+        # Leitura do arquivo Parquet
+        df = spark.read.parquet(pathSource).withColumn("app", regexp_extract(input_file_name(), "/appleStore/(.*?)/odate=", 1))
 
-            # Processamento dos dados
-            df_processado = processing_reviews(df)
+        # Processamento dos dados
+        df_processado = processing_reviews(df)
 
-            # Valida o DataFrame e coleta resultados
-            valid_df, invalid_df, validation_results = validate_ingest(spark, df_processado)
+        # Valida o DataFrame e coleta resultados
+        valid_df, invalid_df, validation_results = validate_ingest(spark, df_processado)
 
-            valid_df.show(50, truncate=False)
-            invalid_df.show(50, truncate=False)
+        valid_df.show(50, truncate=False)
+        invalid_df.show(50, truncate=False)
 
-            # Salvar dados válidos
-            save_dataframe(valid_df, path_target, "valido")
+        # Salvar dados válidos
+        save_dataframe(valid_df, path_target, "valido")
 
-            # Salvar dados inválidos
-            save_dataframe(invalid_df, path_target_fail, "invalido")
+        # Salvar dados inválidos
+        save_dataframe(invalid_df, path_target_fail, "invalido")
 
-            metrics_collector.end_collection()
+        metrics_collector.end_collection()
 
-            # Coleta métricas após o processamento
-            metrics_json = metrics_collector.collect_metrics(valid_df, invalid_df, validation_results, "silver_apple_store")
-            
-            # Salvar métricas no MongoDB
-            save_metrics(spark, metrics_json)
+        # Coleta métricas após o processamento
+        metrics_json = metrics_collector.collect_metrics(valid_df, invalid_df, validation_results, "silver_apple_store")
 
-            logging.info(f"[*] Métricas da aplicação: {metrics_json}")
+        # Salvar métricas no MongoDB
+        save_metrics(spark, metrics_json)
+
+        logging.info(f"[*] Métricas da aplicação: {metrics_json}")
 
     except Exception as e:
         logging.error(f"[*] An error occurred: {e}", exc_info=True)
